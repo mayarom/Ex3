4ee713c4efcd17cef1055b2ebd1fbccfc473601c
maya <maya5660@gmail.com>
2022-12-22 23:39:43 +0200

    lastlastverylast

--- mystack.c
+++ mystack.c
@@ -6,36 +6,43 @@ node_t *first = NULL;
 
 void push(long *timeInMicroSeconds, int *interationCounter, int *cubic_is_0_reno_is_1)
 {
-    node_t *newN = malloc(sizeof(node_t));
-    newN->timeInMicroSeconds = timeInMicroSeconds;
-    newN->interationCounter = interationCounter;
-    newN->cubic_is_0_reno_is_1 = cubic_is_0_reno_is_1;
-    newN->nextN = NULL;
-    if (first == NULL)
+    node_t *new_node = malloc(sizeof(node_t));
+    if (new_node == NULL)
     {
-        first = newN;
+        // Handle the case where malloc fails to allocate memory
+        return;
     }
-    else
-    {
-        newN->nextN = first;
-        first = newN;
-    }
+
+    new_node->timeInMicroSeconds = timeInMicroSeconds;
+    new_node->interationCounter = interationCounter;
+    new_node->cubic_is_0_reno_is_1 = cubic_is_0_reno_is_1;
+    new_node->nextN = first;
+    first = new_node;
 }
 
-long *pop() {
-    if (first == NULL) {
+long *pop()
+{
+    if (first == NULL)
+    {
         return NULL;
     }
 
     long *result = first->timeInMicroSeconds;
 
     // Print the algorithm name and iteration number
-    if (*(first->cubic_is_0_reno_is_1) == 0) {
+    switch (*(first->cubic_is_0_reno_is_1))
+    {
+    case 0:
         printf("algorithm is: cubic iter number: %d time: %ld micro_sec \n",
-        *(first->interationCounter), *(first->timeInMicroSeconds));
-    } else if (*(first->cubic_is_0_reno_is_1) == 1) {
-        printf("algorithm is: reno iter number: %d time: %ld micro_sec  \n",
-        *(first->interationCounter), *(first->timeInMicroSeconds));
+               *(first->interationCounter), *(first->timeInMicroSeconds));
+        break;
+    case 1:
+        printf("algorithm is: reno iter number: %d time: %ld micro_sec \n",
+               *(first->interationCounter), *(first->timeInMicroSeconds));
+        break;
+    default:
+        // Handle the case where the value of *(first->cubic_is_0_reno_is_1) is not 0 or 1
+        break;
     }
 
     // Save a pointer to the current first node
